98b1768d9ce9b7aa91cbef251559cf6b704078a1
Purnima Pandey <purnimapnd04@gmail.com>
2026-03-13 07:40:03 +0530

    Add legend for slot status

--- lib/screens/doctor_home_screen.dart
+++ lib/screens/doctor_home_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:doc_appoint_frontend/models/slot.dart';
 import 'package:doc_appoint_frontend/services/api_service.dart';
+import 'package:doc_appoint_frontend/widgets/slot_generation_dialog.dart';
 import 'package:flutter/material.dart';
 
 class DoctorHomeScreen extends StatefulWidget {
@@ -40,34 +41,30 @@ class _DoctorHomeScreenState extends State<DoctorHomeScreen> {
   }
 
   Widget buildLegend() {
-  return Padding(
-    padding: const EdgeInsets.symmetric(vertical: 10),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        legendItem("Available", const Color(0xFF4CAF7A)),
-        legendItem("Booked", const Color(0xFFE57373)),
-        legendItem("Frozen", const Color(0xFFB0BEC5)),
-        legendItem("Completed", const Color(0xFF5C9ED8)),
-      ],
-    ),
-  );
-}
-
-Widget legendItem(String label, Color color) {
-  return Row(
-    children: [
-      Container(
-        width: 15,
-        height: 15,
-        color: color,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          legendItem("Available", const Color(0xFF4CAF7A)),
+          legendItem("Booked", const Color(0xFFE57373)),
+          legendItem("Frozen", const Color(0xFFB0BEC5)),
+          legendItem("Completed", const Color(0xFF5C9ED8)),
+        ],
       ),
-      const SizedBox(width: 5),
-      Text(label),
-    ],
-  );
-}
-  
+    );
+  }
+
+  Widget legendItem(String label, Color color) {
+    return Row(
+      children: [
+        Container(width: 15, height: 15, color: color),
+        const SizedBox(width: 5),
+        Text(label),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -103,58 +100,65 @@ Widget legendItem(String label, Color color) {
               ),
             ),
 
-            if(isLoading) const Center(child: CircularProgressIndicator(),)
-            else if(slots.isEmpty) const Center(child: Text("No slots generated yet"),)
+            if (isLoading)
+              const Center(child: CircularProgressIndicator())
+            else if (slots.isEmpty)
+              const Center(child: Text("No slots generated yet"))
             else
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 250,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 1.5,
-                ),
-                itemCount: slots.length,
-                itemBuilder: (context, index) {
-                  final slot = slots[index];
-                  final status = slots[index].status;
-                  return Card(
-                    margin: const EdgeInsets.symmetric(
-                      vertical: 4,
-                      horizontal: 5,
-                    ),
-                    color: (status == "available")
-                        ? const Color(0xFF4CAF7A)
-                        : (status == "booked")
-                        ? const Color(0xFFE57373)
-                        : (status == "frozen")
-                        ? const Color(0xFFB0BEC5)
-                        : const Color(0xFF5C9ED8),
-                    child: ListTile(
-                      title: Text(
-                        "${slot.startTime} - ${slot.endTime}",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 250,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 1.5,
+                  ),
+                  itemCount: slots.length,
+                  itemBuilder: (context, index) {
+                    final slot = slots[index];
+                    final status = slots[index].status;
+                    return Card(
+                      margin: const EdgeInsets.symmetric(
+                        vertical: 4,
+                        horizontal: 5,
+                      ),
+                      color: (status == "available")
+                          ? const Color(0xFF4CAF7A)
+                          : (status == "booked")
+                          ? const Color(0xFFE57373)
+                          : (status == "frozen")
+                          ? const Color(0xFFB0BEC5)
+                          : const Color(0xFF5C9ED8),
+                      child: ListTile(
+                        title: Text(
+                          "${slot.startTime} - ${slot.endTime}",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
+                        subtitle: Text(
+                          "Status: $status",
+                          style: TextStyle(fontSize: 15, color: Colors.white),
+                        ),
+                        onTap: () {},
                       ),
-                      subtitle: Text(
-                        "Status: $status",
-                        style: TextStyle(fontSize: 15, color: Colors.white),
-                      ),
-                      onTap: () {},
-                    ),
-                  );
-                },
-              ),              
-            ),
-            
+                    );
+                  },
+                ),
+              ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) =>
+                GenerateSlotsDialog(onSlotsGenerated: fetchSlots),
+          );
+        },
         tooltip: "Create slots",
         backgroundColor: Colors.white,
         foregroundColor: const Color.fromARGB(255, 6, 24, 39),
