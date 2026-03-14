a6d011c33cb9a083db63a5f6a977cbedf2beca0b
Purnima Pandey <purnimapnd04@gmail.com>
2026-03-14 11:41:52 +0530

    Generate slots with breaks and duration

--- lib/widgets/slot_generation_dialog.dart
+++ lib/widgets/slot_generation_dialog.dart
@@ -14,19 +14,23 @@ class _GenerateSlotsDialogState extends State<GenerateSlotsDialog> {
   final startController = TextEditingController();
   final endController = TextEditingController();
 
-  Future<void> generateSlots() async {
+  final durationController = TextEditingController();
+  final breakStartController = TextEditingController();
+  final breakEndController = TextEditingController();
+
+  Future<void> generateSlots({
+    required List<Map<String, String>> breaks,
+    required int slotDuration,
+  }) async {
     final today = DateTime.now().toIso8601String().split("T")[0];
 
-    final response = await ApiService().postRequest(
-      "/slots/generate",
-      {
-        "date": today,
-        "day_start": startController.text,
-        "day_end": endController.text,
-        "slot_duration_minutes": 30,
-        "breaks": []
-      },
-    );
+    final response = await ApiService().postRequest("/slots/generate", {
+      "date": today,
+      "day_start": startController.text,
+      "day_end": endController.text,
+      "slot_duration_minutes": slotDuration,
+      "breaks": breaks,
+    });
 
     if (response.statusCode == 200) {
       widget.onSlotsGenerated();
@@ -42,6 +46,16 @@ class _GenerateSlotsDialogState extends State<GenerateSlotsDialog> {
   }
 
   @override
+  void dispose() {
+    startController.dispose();
+    endController.dispose();
+    durationController.dispose();
+    breakStartController.dispose();
+    breakEndController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text("Generate Slots"),
@@ -50,11 +64,42 @@ class _GenerateSlotsDialogState extends State<GenerateSlotsDialog> {
         children: [
           TextField(
             controller: startController,
-            decoration: const InputDecoration(labelText: "Start Time (09:00)"),
+            decoration: const InputDecoration(
+              labelText: "Start Time",
+              hintText: "(09:00)",
+            ),
           ),
+          const SizedBox(height: 7),
           TextField(
             controller: endController,
-            decoration: const InputDecoration(labelText: "End Time (12:00)"),
+            decoration: const InputDecoration(
+              labelText: "End Time",
+              hintText: "(12:00)",
+            ),
+          ),
+          const SizedBox(height: 7),
+          TextField(
+            controller: durationController,
+            decoration: const InputDecoration(
+              labelText: "Duration in minutes",
+              hintText: "20",
+            ),
+          ),
+          const SizedBox(height: 7),
+          TextField(
+            controller: breakStartController,
+            decoration: const InputDecoration(
+              labelText: " Break Start Time",
+              hintText: "(13:00)",
+            ),
+          ),
+          const SizedBox(height: 7),
+          TextField(
+            controller: breakEndController,
+            decoration: const InputDecoration(
+              labelText: "Break End Time",
+              hintText: "(14:00)",
+            ),
           ),
         ],
       ),
@@ -64,7 +109,21 @@ class _GenerateSlotsDialogState extends State<GenerateSlotsDialog> {
           child: const Text("Cancel"),
         ),
         ElevatedButton(
-          onPressed: generateSlots,
+          onPressed: () {
+            generateSlots(
+              slotDuration: int.parse(durationController.text),
+              breaks:
+                  breakStartController.text.isNotEmpty &&
+                      breakEndController.text.isNotEmpty
+                  ? [
+                      {
+                        "start": breakStartController.text,
+                        "end": breakEndController.text,
+                      },
+                    ]
+                  : [],
+            );
+          },
           child: const Text("Generate"),
         ),
       ],
