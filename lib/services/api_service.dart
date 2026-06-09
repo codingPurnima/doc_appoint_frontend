db6140aa7917c0bb32dc73e08ae425cfe50e2e55
Purnima Pandey <purnimapnd04@gmail.com>
2026-06-09 12:25:35 +0530

    Connect deployed backend

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'auth_service.dart';
 
 class ApiService {
-  static const String baseUrl = "http://10.0.2.2:8000";
+  static const String baseUrl = "https://doc-appoint-backend-meb4.onrender.com";
 
   Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
