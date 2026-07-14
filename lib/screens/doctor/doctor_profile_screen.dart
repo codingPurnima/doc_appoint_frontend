320a7e61d17e01e520fe4eee3e52f7ceca2e9dfe
Purnima Pandey <purnimapnd04@gmail.com>
2026-07-14 21:20:07 +0530

    refactor: separate business logic from ui

--- lib/screens/doctor/doctor_profile_screen.dart
+++ lib/screens/doctor/doctor_profile_screen.dart
@@ -1,97 +1,48 @@
-import 'dart:convert';
 
-import 'package:doc_appoint_frontend/screens/common/login_screen.dart';
+import 'package:doc_appoint_frontend/providers/doctor_profile_provider.dart';
 import 'package:doc_appoint_frontend/screens/common/profile_screen.dart';
-import 'package:doc_appoint_frontend/services/api_service.dart';
-import 'package:doc_appoint_frontend/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class DoctorProfileScreen extends StatefulWidget {
+class DoctorProfileScreen extends ConsumerWidget {
   const DoctorProfileScreen({super.key});
 
-  @override
-  State<DoctorProfileScreen> createState() => _DoctorProfileScreenState();
-}
-
-class _DoctorProfileScreenState extends State<DoctorProfileScreen> {
-  Map<String, dynamic>? user;
-  List<dynamic> appointments = [];
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    fetchProfileData();
-  }
-
-  Future<void> fetchProfileData() async {
-    final api = ApiService();
-    final userResponse = await api.getRequest("/users/me");
-    final appointmentResponse = await api.getRequest("/appointments/doctor");
-
-    if (userResponse.statusCode == 200 &&
-        appointmentResponse.statusCode == 200) {
-      setState(() {
-        user = jsonDecode(userResponse.body);
-        appointments = jsonDecode(appointmentResponse.body);
-        isLoading = false;
-      });
-    } else if (userResponse.statusCode == 401) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            "Oops! Seems like your session expired. Please login again",
+  Future<void> completeAppointment(
+    BuildContext context,
+    WidgetRef ref,
+    int appointmentId,
+  ) async {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Complete Appointment?"),
+        content: const Text("Mark this appointment as completed?"),
+        actions: [
+          TextButton(
+            onPressed: () async {
+              Navigator.pop(context);
+              final status = await ref
+                  .read(doctorProfileProvider.notifier)
+                  .completeAppointment(appointmentId);
+              if (!context.mounted) return;
+              if (status == 200) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Appointment completed")),
+                );
+              }
+            },
+            child: const Text("Yes"),
           ),
-        ),
-      );
-      await AuthService().logout();
-      if (!mounted) return;
-      Navigator.pushAndRemoveUntil(
-        context,
-        MaterialPageRoute(builder: (context) => LoginScreen()),
-        (route) => false,
-      );
-      return;
-    } else {
-      setState(() {
-        isLoading = false;
-      });
-    }
+          ElevatedButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("No"),
+          ),
+        ],
+      ),
+    );
   }
 
-  Future<void> completeAppointment(int appointmentId) async {
-  showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: const Text("Complete Appointment?"),
-      content: const Text("Mark this appointment as completed?"),
-      actions: [
-        TextButton(
-          onPressed: () async {
-            final response = await ApiService().patchRequest(
-              "/appointments/$appointmentId/complete",
-              {},
-            );
-            Navigator.pop(context);
-            if (response.statusCode == 200) {
-              fetchProfileData();
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text("Appointment completed")),
-              );
-            }
-          },
-          child: const Text("Yes"),
-        ),
-        ElevatedButton(
-          onPressed: () => Navigator.pop(context),
-          child: const Text("No"),
-        ),
-      ],
-    ),
-  );
-}
-
-  Future<void> logout() async {
+  Future<void> logout(BuildContext context, WidgetRef ref) async {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
@@ -100,7 +51,7 @@ class _DoctorProfileScreenState extends State<DoctorProfileScreen> {
         actions: [
           TextButton(
             onPressed: () async {
-              await AuthService().logout();
+              await ref.read(doctorProfileProvider.notifier).logout();
               if (!context.mounted) return;
               Navigator.pop(context);
               Navigator.pushNamedAndRemoveUntil(context, "/", (route) => false);
@@ -117,14 +68,35 @@ class _DoctorProfileScreenState extends State<DoctorProfileScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(doctorProfileProvider);
+    ref.listen(doctorProfileProvider, (previous, next) async {
+      if (next.error == "session_expired") {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text(
+              "Oops! Seems like your session expired. Please login again.",
+            ),
+          ),
+        );
+        await ref.read(doctorProfileProvider.notifier).logout();
+
+        if (!context.mounted) return;
+
+        Navigator.pushNamedAndRemoveUntil(
+          context,
+          "/",
+          (_) => false,
+        );
+      }
+    });
     return ProfileScreen(
-      user: user,
-      appointments: appointments,
-      isLoading: isLoading,
-      onLogout: logout,
+      user: state.user,
+      appointments: state.appointments,
+      isLoading: state.isLoading,
+      onLogout: ()=> logout(context, ref),
       title: "Doctor Profile",
-      onCancelOrCompleteAppointment: completeAppointment,
+      onCancelOrCompleteAppointment: (id)=> completeAppointment(context, ref, id),
     );
   }
 }
