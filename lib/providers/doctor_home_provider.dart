e17d3b930900bfe8bba15f2ba54d88eb33235223
Purnima Pandey <purnimapnd04@gmail.com>
2026-09-10 10:24:53 +0530

    remove unused import

--- lib/providers/doctor_home_provider.dart
+++ lib/providers/doctor_home_provider.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/slot.dart';
 import '../services/api_service.dart';
